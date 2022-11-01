b61db15b482c5a93dcb5be05bbe29541b063dbef
plm <pierrelouis.mazoyer@diiage.org>
2022-11-01 09:20:53 +0100

    Fix commit issue

---
+++ XStory/XStory/Helpers/Constants/SettingsPageConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XStory.Helpers.Constants
+{
+    public class SettingsPageConstants
+    {
+        public const string SETTINGS_TITLE = "Settings";
+    }
+}
---
+++ XStory/XStory/Helpers/Converters/MainPageChapterNameConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+using XStory.DTO;
+
+namespace XStory.Helpers.Converters
+{
+    /// <summary>
+    /// On MainPage : Defines chapter name field visible or not if no value
+    /// </summary>
+    public class MainPageChapterNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if((value as string) == string.Empty)
+            {
+                
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
