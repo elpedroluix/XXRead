866a820c3c5f82cf4bd56505da965e8f18a63956
plm <pierrelouis.mazoyer@diiage.org>
2022-11-15 23:11:38 +0100

    MainPage display OK + SettingsPage improvement for Categories (not yet implemented)

--- XStory/XStory/ViewModels/MainPageViewModel.cs
+++ XStory/XStory/ViewModels/MainPageViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Xamarin.Forms;
 using XStory.DTO;
 
 namespace XStory.ViewModels
@@ -13,6 +14,8 @@ namespace XStory.ViewModels
     {
         private List<Story> _stories;
         private BL.Web.Contracts.IServiceStory _serviceStory;
+
+        private StackLayout _storiesContainer;
 
         public DelegateCommand<string> StoriesItemTappedCommand { get; set; }
         public DelegateCommand StoriesItemAppearingCommand { get; set; }
@@ -25,10 +28,18 @@ namespace XStory.ViewModels
             set { SetProperty(ref _stories, value); }
         }
 
+        public StackLayout StoriesContainer
+        {
+            get { return _storiesContainer; }
+            set { SetProperty(ref _storiesContainer, value); }
+        }
+
         public MainPageViewModel(INavigationService navigationService, BL.Web.Contracts.IServiceStory serviceStory)
             : base(navigationService)
         {
             Title = "Main Page";
+
+            StoriesContainer = new StackLayout();
 
             AppearingCommand = new DelegateCommand(ExecuteAppearingCommand);
             SettingsCommand = new DelegateCommand(ExecuteSettingsCommand);
@@ -36,6 +47,23 @@ namespace XStory.ViewModels
             StoriesItemAppearingCommand = new DelegateCommand(ExecuteStoriesItemAppearingCommand);
 
             _serviceStory = serviceStory;
+        }
+
+        private void MakeStoriesContainer()
+        {
+            // Clears the Grid
+            StoriesContainer.Children.Clear();
+            //StoriesContainer.RowDefinitions.Add(new RowDefinition());
+            StoriesContainer.Children.Add(new Label() { Text = "lol" });
+
+            //// Create a StoryInfoView (of type ContentView) and add it to the grid
+            //for (int i = 0; i < Stories.Count; i++)
+            //{
+            //    Views.ContentViews.StoryView storyView = new Views.ContentViews.StoryView();
+            //    storyView.BindingContext = Stories[i];
+
+            //    StoriesContainer.Children.Add(storyView, 0, i + 1);
+            //}
         }
 
         private void ExecuteStoriesItemAppearingCommand()
@@ -61,6 +89,8 @@ namespace XStory.ViewModels
         protected override async void ExecuteAppearingCommand()
         {
             Stories = await _serviceStory.GetStoriesMainPage(0, "");
+
+            MakeStoriesContainer();
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
--- XStory/XStory/ViewModels/SettingsPageViewModel.cs
+++ XStory/XStory/ViewModels/SettingsPageViewModel.cs
@@ -9,20 +9,64 @@ namespace XStory.ViewModels
 {
     public class SettingsPageViewModel : BaseViewModel
     {
+        private Xamarin.Forms.FlexLayout _categoriesContentView;
 
-        public DelegateCommand CacaCommand { get; set; }
+        public Xamarin.Forms.FlexLayout CategoriesContentView
+        {
+            get { return _categoriesContentView; }
+            set { SetProperty(ref _categoriesContentView, value); }
+        }
+
+        private Xamarin.Forms.ContentView _themesContentView;
+
+        public Xamarin.Forms.ContentView ThemesContentView
+        {
+            get { return _themesContentView; }
+            set { SetProperty(ref _themesContentView, value); }
+        }
+
+
+        public DelegateCommand<string> CacaCommand { get; set; }
+        public DelegateCommand<string> SelectedCategoryCommand { get; set; }
 
         public SettingsPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             Title = Helpers.Constants.SettingsPageConstants.SETTINGS_TITLE;
 
-            CacaCommand = new DelegateCommand(ExecuteCacaCommand);
+            CacaCommand = new DelegateCommand<string>((color) => ExecuteCacaCommand(color));
+            SelectedCategoryCommand = new DelegateCommand<string>((boolimie) => ExecuteSelectedCategoryCommand(boolimie));
+
+            // BuildCategoriesSettings();
         }
 
-        private void ExecuteCacaCommand()
+        private void ExecuteSelectedCategoryCommand(string boolimie)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteCacaCommand(string color)
         {
             Title = "lolol";
+
+            // CategoriesContentView.Children[0].BackgroundColor = Xamarin.Forms.Color.Brown;
+        }
+
+        private void BuildCategoriesSettings()
+        {
+            CategoriesContentView = new Xamarin.Forms.FlexLayout()
+            {
+                Direction = Xamarin.Forms.FlexDirection.Row,
+                Wrap = Xamarin.Forms.FlexWrap.Wrap,
+                Children =
+                    {
+                        new Xamarin.Forms.Button() { Text = "Gay" },
+                        new Xamarin.Forms.Button() { Text = "Hétéro crap 1" },
+                        new Xamarin.Forms.Button() { Text = "Hétéro / c nul 2" },
+                        new Xamarin.Forms.Button() { Text = "Envie de sucer de la grosse teub" },
+                        new Xamarin.Forms.Button() { Text = "J'suis une pute" }
+                    }
+            };
         }
     }
 }
