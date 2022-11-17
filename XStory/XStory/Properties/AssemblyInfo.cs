75d6219ba77c921d529333d7f1077eb026c6b103
plm <pierrelouis.mazoyer@diiage.org>
2022-11-17 23:02:01 +0100

    Added Georgia Font + styles

--- XStory/XStory/Properties/AssemblyInfo.cs
+++ XStory/XStory/Properties/AssemblyInfo.cs
@@ -1,3 +1,6 @@
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+
+[assembly: ExportFont("Georgia-Regular-font.ttf", Alias = "Georgia")]
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
