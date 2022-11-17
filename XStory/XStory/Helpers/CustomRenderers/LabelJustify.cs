12e9545251cfe932661c2365a0ace7a3d520f995
plm <pierrelouis.mazoyer@diiage.org>
2022-11-17 22:19:50 +0100

    Justify text StoryPage

---
+++ XStory/XStory/Helpers/CustomRenderers/LabelJustify.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Behaviors;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace XStory.Helpers.CustomRenderers
+{
+    public class LabelJustify : Xamarin.Forms.Label
+    {
+        public static readonly BindableProperty JustifyTextProperty =
+            BindableProperty.Create("JustifyText", typeof(bool), typeof(Label), false);
+
+        public bool JustifyText
+        {
+            get { return (bool)GetValue(JustifyTextProperty); }
+            set { SetValue(JustifyTextProperty, value); }
+        }
+    }
+}
