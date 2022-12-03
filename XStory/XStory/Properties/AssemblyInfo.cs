52097a883edb5bc9762cef481268eaf317f1ed31
plm <pierrelouis.mazoyer@diiage.org>
2022-12-03 17:56:25 -0400

    Changed font to Tahoma

--- XStory/XStory/Properties/AssemblyInfo.cs
+++ XStory/XStory/Properties/AssemblyInfo.cs
@@ -2,5 +2,6 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: ExportFont("Georgia-Regular-font.ttf", Alias = "Georgia")]
+[assembly: ExportFont("Tahoma-Regular-font.ttf", Alias = "Tahoma")]
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
