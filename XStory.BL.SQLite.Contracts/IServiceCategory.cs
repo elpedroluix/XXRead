fa8a5b53e7f6cada3a5de65bdd723473c45bccf1
plm <pierrelouis.mazoyer@diiage.org>
2023-01-18 12:36:33 -0400

    added SQLite ServiceCategory

---
+++ XStory.BL.SQLite.Contracts/IServiceCategory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XStory.BL.SQLite.Contracts
+{
+    public interface IServiceCategory
+    {
+        Task<List<DTO.Category>> GetCategories();
+        Task<DTO.Category> GetCategory(string url);
+        Task<int> InsertCategory(DTO.Category category);
+    }
+}
---
+++ XStory.BL.SQLite/ServiceCategory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using XStory.BL.SQLite.Contracts;
+using XStory.DAL.SQLite;
+using XStory.DAL.SQLite.Contracts;
+using XStory.DTO;
+
+namespace XStory.BL.SQLite
+{
+    public class ServiceCategory : IServiceCategory
+    {
+        private IRepositoryCategory _repositoryCategory;
+
+        public ServiceCategory()
+        {
+            _repositoryCategory = new RepositoryCategory();
+        }
+
+        public Task<List<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Category> GetCategory(string url)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> InsertCategory(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
